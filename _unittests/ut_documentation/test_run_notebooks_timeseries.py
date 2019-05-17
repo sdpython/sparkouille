52544480fae9147801aac37cb2edc7ab8d5cc15a
xavier dupré <xavier.dupre@gmail.com>
2019-05-17 14:19:50 +0200

    split unit test

--- _unittests/ut_documentation/test_run_notebooks_timeseries.py
+++ _unittests/ut_documentation/test_run_notebooks_timeseries.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-@brief      test log(time=80s)
+@brief      test log(time=134s)
 """
 import os
 import unittest
@@ -10,13 +10,13 @@ from pyquickhelper.pycode import add_missing_development_version
 import sparkouille
 
 
-class TestFunctionTestNotebookSql(unittest.TestCase):
+class TestFunctionTestNotebookTimeSeries(unittest.TestCase):
 
     def setUp(self):
         add_missing_development_version(
             ["jyquickhelper", "pymyinstall"], __file__, hide=True)
 
-    def test_notebook_sql_map_reduce(self):
+    def test_notebook_map_reduce_timeseries(self):
         fLOG(
             __file__,
             self._testMethodName,
@@ -26,7 +26,7 @@ class TestFunctionTestNotebookSql(unittest.TestCase):
         self.assertTrue(sparkouille is not None)
         folder = os.path.join(os.path.dirname(__file__),
                               "..", "..", "_doc", "notebooks", "sql")
-        test_notebook_execution_coverage(__file__, "sql_map_reduce", folder,
+        test_notebook_execution_coverage(__file__, "map_reduce_timeseries", folder,
                                          this_module_name="sparkouille", fLOG=fLOG,
                                          copy_files=["README.txt"], modules=[pymyinstall])
 
