8b6c9daefb1e990c8bbc1552c52f591e2dd43d87
xavier dupré <xavier.dupre@gmail.com>
2021-01-22 00:28:15 +0100

    documentation and requirements

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -3,7 +3,7 @@ import sys
 import os
 # import sphinx_redactor_theme
 # import karma_sphinx_theme
-import sphinx_bootstrap_theme
+import pydata_sphinx_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 
 
@@ -11,8 +11,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 local_template = os.path.join(os.path.abspath(
     os.path.dirname(__file__)), "phdoc_templates")
 
-set_sphinx_variables(__file__, "sparkouille", "Xavier Dupré", 2020,
-                     "bootstrap", sphinx_bootstrap_theme.get_html_theme_path(),
+set_sphinx_variables(__file__, "sparkouille", "Xavier Dupré", 2021,
+                     "pydata_sphinx_theme", pydata_sphinx_theme.get_html_theme_path(),
                      locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/sparkouille/issues/%s', 'issue')),
                      title="sparkouille", book=True)
